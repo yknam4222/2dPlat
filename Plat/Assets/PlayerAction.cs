9ce0bec5a9f6f948b43b4f93d02ce08d3f436c9d
yknam4222 <yyhnam2222@gmail.com>
2023-04-24 13:49:23 +0900

    NamA24

---
+++ Plat/Assets/PlayerAction.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    float h;
+    float v;
+
+    Rigidbody2D rigid;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(h, v);
+    }
+}
