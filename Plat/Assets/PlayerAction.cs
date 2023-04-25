fc42251923e6ee592abdd9cfe11e94187ba37c6f
yknam4222 <yyhnam2222@gmail.com>
2023-04-25 11:36:37 +0900

    nam

--- Plat/Assets/PlayerAction.cs
+++ Plat/Assets/PlayerAction.cs
@@ -4,23 +4,77 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
+    public float speed;
     float h;
     float v;
+    bool isHorizonMove;
+    Vector3 dirVec;
+    GameObject scanObject;
 
     Rigidbody2D rigid;
+    Animator anim;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        if (hDown)
+            isHorizonMove = true;
+        else if (vDown)
+            isHorizonMove = false;
+        else if (hUp || vUp)
+            isHorizonMove = h != 0;
+
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+            anim.SetBool("isChange", false);
+
+        if (vDown && v == 1)
+            dirVec = Vector3.up;
+        else if (vDown && v == -1)
+            dirVec = Vector3.down;
+        else if (hDown && h == -1)
+            dirVec = Vector3.left;
+        else if (hDown && h == 1)
+            dirVec = Vector3.right;
+
+        if (Input.GetButtonDown("Jump") && scanObject != null)
+            Debug.Log(scanObject.name);
     }
 
     private void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v);
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * speed;
+
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+
+        if (rayHit.collider != null)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+            scanObject = null;
     }
 }
